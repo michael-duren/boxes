230d40cd86b324ecdcc81d9627c31e6446419664
Michael Duren <michaeld@michaelduren.com>
2026-06-15 09:01:13 -0500

    feat: add logging for ops & kill ops

--- internal/operations/kill.go
+++ internal/operations/kill.go
@@ -1,6 +1,14 @@
 package operations
 
-import "fmt"
+import (
+	"fmt"
+	"log/slog"
+	"strconv"
+
+	"golang.org/x/sys/unix"
+
+	"github.com/michael-duren/boxes/internal/container"
+)
 
 type KillOpts struct {
 	ID     string
@@ -8,7 +16,30 @@ type KillOpts struct {
 }
 
 func Kill(opts *KillOpts) error {
-	fmt.Println(opts)
+	slog.Info("kill operation", "id", opts.ID, "signal", opts.Signal)
 
+	cntr, err := container.Load(opts.ID)
+	if err != nil {
+		slog.Error("failed to load container", "id", opts.ID, "err", err)
+		return fmt.Errorf("load container: %w", err)
+	}
+
+	sig, err := strconv.Atoi(opts.Signal)
+	if err != nil {
+		slog.Error("failed to convert signal to int", "id", opts.ID, "signal", opts.Signal, "err", err)
+		return fmt.Errorf("convert signal to int: %w", err)
+	}
+
+	if err := cntr.Kill(unix.Signal(sig)); err != nil {
+		slog.Error("failed to kill container", "id", opts.ID, "signal", sig, "err", err)
+		return fmt.Errorf("kill container: %w", err)
+	}
+
+	if err := cntr.Save(); err != nil {
+		slog.Error("failed to save container", "id", opts.ID, "err", err)
+		return fmt.Errorf("save container: %w", err)
+	}
+
+	slog.Info("kill operation complete", "id", opts.ID, "signal", sig)
 	return nil
 }
--- internal/operations/start.go
+++ internal/operations/start.go
@@ -2,6 +2,7 @@ package operations
 
 import (
 	"fmt"
+	"log/slog"
 
 	"github.com/michael-duren/boxes/internal/container"
 )
@@ -11,19 +12,25 @@ type StartOpts struct {
 }
 
 func Start(opts *StartOpts) error {
+	slog.Info("start operation", "id", opts.ID)
+
 	cntr, err := container.Load(opts.ID)
 
 	if err != nil {
+		slog.Error("failed to load container", "id", opts.ID, "err", err)
 		return fmt.Errorf("load container: %w", err)
 	}
 
 	if err := cntr.Start(); err != nil {
+		slog.Error("failed to start container", "id", opts.ID, "err", err)
 		return fmt.Errorf("start container: %w", err)
 	}
 
 	if err := cntr.Save(); err != nil {
+		slog.Error("failed to save container", "id", opts.ID, "err", err)
 		return fmt.Errorf("save container: %w", err)
 	}
 
+	slog.Info("start operation complete", "id", opts.ID)
 	return nil
 }
